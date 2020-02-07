ee73b158257fe21a8823d3d5c92b7d35231d5aa5
yuuy <yuuyluna@outlook.com>
2020-02-07 20:10:57 +0800

    Update Is your birthday contained in the first million digits of Pi.py

--- Is your birthday contained in the first million digits of Pi.py
+++ Is your birthday contained in the first million digits of Pi.py
@@ -1,8 +1,9 @@
-# 函数open（），参数为需要打开的文件的名称
+# 函数open（），第一个实参为需要打开的文件的名称，第二个实参为模式（r, w, a, r+ mode)
 # 关键词with，在不再需要访问文件后将其关闭
 # 方法read(),读取文件全部内容
 # 方法readlines()，从文件中读取每一行，并将其存储在一个列表中
 file_path = r'C:\Users\HP\Documents\Python_Work\text_files\pi_million_digits.txt'
+
 with open(file_path) as file_object:
     lines = file_object.readlines()
 
